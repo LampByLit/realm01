0b51c5e90af81fe2e7d0aefdaacf813c9c2fc982
LampByLit <k.ryan.hartley@gmail.com>
2025-11-13 23:43:20 -0800

    again

--- drugwars-main/src/components/input/input.styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const InputBase = styled.input`
-  text-transform: uppercase;
-  padding: 0.25em 1em;
-  border-radius: 2px;
-  border: 2px solid ${({ theme }) => theme.colors.primary};
-`;
